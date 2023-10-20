270e0aa3b86abb225f91e8f80aa8d5449c75778d
jazzkrop <jazzkrop@gmail.com>
2023-10-20 07:13:41 +0300

    refactor task list, create Task Form

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,25 +1,29 @@
 import 'package:do_important_zanovo/src/sample_feature/sample_item_simple_view.dart';
 import 'package:do_important_zanovo/src/sample_feature/value.dart';
 import 'package:do_important_zanovo/src/sample_feature/widgets/modal_auth.dart';
+import 'package:do_important_zanovo/src/settings/settings_view.dart';
 import 'package:do_important_zanovo/src/widgets/screen_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 import 'sample_item.dart';
 
 /// Displays a list of SampleItems.
 class SampleItemListView extends StatefulWidget {
-  SampleItemListView({super.key});
+  SampleItemListView({super.key, this.controller});
+
+  final controller;
 
   static const routeName = '/';
 
   List<SampleItem> items = [
     SampleItem(
-        id: '1',
-        createdAt: DateTime.now(),
-        importance: 1,
-        title: 'УМ підготуватись до тесту, бо буде бобо, а потім син...',
-        reason: 'якісний пастор гарно говорить',
-        valuesIds: ['1']),
+      id: '1',
+      createdAt: DateTime.now(),
+      importance: 1,
+      title: 'спитати шопського за допомогу медичну фінансову',
+      reason: 'якісний пастор гарно говорить',
+    ),
     SampleItem(
       id: '2',
       createdAt: DateTime.now().add(const Duration(hours: 2)),
@@ -41,6 +45,34 @@ class SampleItemListView extends StatefulWidget {
       title: 'О душ',
       reason: 'бути чесним це класно',
     ),
+    SampleItem(
+        id: '5',
+        createdAt: DateTime.now(),
+        importance: 1,
+        title: 'спитати шопського за допомогу медичну фінансову',
+        reason: 'якісний пастор гарно говорить',
+        valuesIds: ['1']),
+    SampleItem(
+      id: '6',
+      createdAt: DateTime.now().add(const Duration(hours: 2)),
+      importance: 2,
+      title: 'Д дочитати ХНС',
+      reason: 'це круто',
+    ),
+    SampleItem(
+      id: '7',
+      createdAt: DateTime.now().add(const Duration(hours: 1)),
+      importance: 3,
+      title: 'З син червяк',
+      reason: 'питає в мами',
+    ),
+    SampleItem(
+      id: '8',
+      createdAt: DateTime.now().add(const Duration(hours: 1)),
+      importance: 4,
+      title: 'О душ',
+      reason: 'бути чесним це класно',
+    ),
   ];
 
   List<Value> values = [Value(id: '1', title: 'гроші')];
@@ -53,14 +85,89 @@ class _SampleItemListViewState extends State<SampleItemListView> {
   void initState() {
     super.initState();
     // modal auth
-    WidgetsBinding.instance.addPostFrameCallback(ModalAuth().main);
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) async => ModalAuth().main(_, context));
   }
 
   @override
   Widget build(BuildContext context) {
+    var topActions = Padding(
+      padding: const EdgeInsets.only(left: 12, right: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            color: Theme.of(context).colorScheme.secondary,
+            icon: const Icon(Icons.question_mark_rounded),
+          ),
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => SettingsView(
+                          controller: widget.controller,
+                        )));
+              },
+              color: Theme.of(context).colorScheme.secondary,
+              icon: const Icon(Icons.settings_sharp)),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: null,
       body: ScreenWrapper(
+        onTapAction: () {
+          var descriptionFocusNode = FocusNode();
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Column(
+                      children: [
+                        TextField(
+                          autofocus: true,
+                          maxLength: 60,
+                          scrollPadding: EdgeInsets.zero,
+                          decoration: const InputDecoration(
+                              alignLabelWithHint: true,
+                              labelText: "що",
+                              contentPadding: EdgeInsets.zero,
+                              hintText: 'шо будеш робити?'),
+                          style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.fontSize,
+                          ),
+                          onSubmitted: (text) {
+                            descriptionFocusNode.requestFocus();
+                          },
+                        ),
+                        TextField(
+                          autofocus: true,
+                          maxLength: 30,
+                          focusNode: descriptionFocusNode,
+                          decoration: const InputDecoration(
+                              alignLabelWithHint: true,
+                              labelText: "чому",
+                              contentPadding: EdgeInsets.zero,
+                              hintText: 'чому ти це робиш?'),
+                          style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.fontSize,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              });
+        },
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(
               decelerationRate: ScrollDecelerationRate.fast),
@@ -70,21 +177,7 @@ class _SampleItemListViewState extends State<SampleItemListView> {
                 const SizedBox(
                   height: 12,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 12, right: 12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.question_mark_rounded),
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.settings_sharp))
-                    ],
-                  ),
-                ),
+                topActions,
                 const SizedBox(
                   height: 24,
                 ),
@@ -102,6 +195,9 @@ class _SampleItemListViewState extends State<SampleItemListView> {
                           key: Key(item.id),
                         ));
                   },
+                ),
+                const SizedBox(
+                  height: 120,
                 ),
               ],
             ),
--- lib/src/sample_feature/widgets/modal_auth.dart
+++ lib/src/sample_feature/widgets/modal_auth.dart
@@ -24,91 +24,93 @@ class ModalAuth {
     return internetConnection;
   }
 
-  void main(_) async {
-    if (FirebaseAuth.instance.currentUser != null) {
-      return;
-    }
-    bool internetConnection = await _checkConnection();
-    // ignore: use_build_context_synchronously
-    bool? dialogResult = await showDialog<bool>(
-        context: _,
-        builder: (context) => StatefulBuilder(
-              builder: (context, setState) {
-                return Dialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const SizedBox(
-                        height: 25,
+  main(_, context) => (_, context) async {
+        if (FirebaseAuth.instance.currentUser != null) {
+          return;
+        }
+        bool internetConnection = await _checkConnection();
+        // ignore: use_build_context_synchronously
+        bool? dialogResult = await showDialog<bool>(
+            context: context,
+            builder: (context) => StatefulBuilder(
+                  builder: (context, setState) {
+                    return Dialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8, right: 8),
-                        child: Text(
-                          'Вхід у акаунт',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                      ),
-                      const SizedBox(height: 32),
-                      internetConnection
-                          ? Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 16.0, right: 16.0),
-                              child: FilledButton.icon(
-                                style: FilledButton.styleFrom(
-                                    minimumSize: const Size.fromHeight(40)),
-                                onPressed: _sign_in_google_on_press,
-                                label: const Text("Sign in with Google"),
-                                icon: Image.asset(
-                                  'assets/images/btn_google_light_normal_xhdpi.9.png',
-                                  fit: BoxFit.cover,
-                                  width: 28,
-                                ),
-                              ),
-                            )
-                          : Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 16.0, right: 16.0),
-                              child: FilledButton(
-                                  style: FilledButton.styleFrom(
-                                      minimumSize: const Size.fromHeight(40)),
-                                  onPressed: () async {
-                                    var newInternetConnection =
-                                        await _checkConnection();
-                                    // кнопка. тепер є інтернет
-                                    setState(() {
-                                      internetConnection =
-                                          newInternetConnection;
-                                    });
-                                  },
-                                  child: const Text(
-                                      'Тепер є доступ до Інтернету')),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, right: 8),
+                            child: Text(
+                              'Вхід у акаунт',
+                              style: Theme.of(context).textTheme.titleLarge,
                             ),
-                      // ignore: prefer_const_constructors
-                      const SizedBox(
-                        height: 12,
+                          ),
+                          const SizedBox(height: 32),
+                          internetConnection
+                              ? Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 16.0, right: 16.0),
+                                  child: FilledButton.icon(
+                                    style: FilledButton.styleFrom(
+                                        minimumSize: const Size.fromHeight(40)),
+                                    onPressed: _sign_in_google_on_press,
+                                    label: const Text("Sign in with Google"),
+                                    icon: Image.asset(
+                                      'assets/images/btn_google_light_normal_xhdpi.9.png',
+                                      fit: BoxFit.cover,
+                                      width: 28,
+                                    ),
+                                  ),
+                                )
+                              : Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 16.0, right: 16.0),
+                                  child: FilledButton(
+                                      style: FilledButton.styleFrom(
+                                          minimumSize:
+                                              const Size.fromHeight(40)),
+                                      onPressed: () async {
+                                        var newInternetConnection =
+                                            await _checkConnection();
+                                        // кнопка. тепер є інтернет
+                                        setState(() {
+                                          internetConnection =
+                                              newInternetConnection;
+                                        });
+                                      },
+                                      child: const Text(
+                                          'Тепер є доступ до Інтернету')),
+                                ),
+                          // ignore: prefer_const_constructors
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 16.0, right: 16.0),
+                            child: FilledButton.tonal(
+                              style: FilledButton.styleFrom(
+                                  minimumSize: const Size.fromHeight(40)),
+                              onPressed: _onClosePressed,
+                              child: const Text('Close'),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                        child: FilledButton.tonal(
-                          style: FilledButton.styleFrom(
-                              minimumSize: const Size.fromHeight(40)),
-                          onPressed: _onClosePressed,
-                          child: const Text('Close'),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ));
-    if (dialogResult == null) {
-      SystemNavigator.pop();
-    }
-  }
+                    );
+                  },
+                ));
+        if (dialogResult == null) {
+          SystemNavigator.pop();
+        }
+      };
 }
